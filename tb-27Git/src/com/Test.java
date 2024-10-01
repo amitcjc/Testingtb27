342df363fdf7410a17552967a4e97dde26d52173
amitcjc <156275076+amitcjc@users.noreply.github.com>
2024-10-01 08:55:31 +0530

    Update Test.java

--- tb-27Git/src/com/Test.java
+++ tb-27Git/src/com/Test.java
@@ -44,7 +44,9 @@ public class Test
 	
 public static void main(String[] args) 
 {
-System.out.println("This is Main Method");	
+System.out.println("This is Main Method");
+	Test t=new Test();
+	t.m1();
 }
 	
 }
